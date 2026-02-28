aa961cc646ee65e07ab6c7de6e2aca6f31ff54c7
yasteria010-code <yasteria010@gmail.com>
2026-02-28 17:51:17 +0800

    Update 5240app.py

--- 5240app.py
+++ 5240app.py
@@ -1,28 +1,56 @@
-import streamlit as st
+
+# import part
 from transformers import pipeline
 from PIL import Image
-
-# Streamlit UI
-st.header("Title: Age Classification using ViT")
-
-# Load the age classification pipeline
-age_classifier = pipeline("image-classification",
-                          model="prithivMLmods/open-age-detection")
-
-image_name = "middleagedMan.jpg"
-image_name = Image.open(image_name).convert("RGB")
-
-# Classify age
-age_predictions = age_classifier(image_name)
-st.write(age_predictions)
-age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
-
-# Display results
-st.write("Predicted Age Range:")
-st.write(f"Age range: {age_predictions[0]['label']}")
-
-st.write("Done")
+import streamlit as st
 
 
-if __name__ == "__main__":
+# function part
+def ageClassifier(imgFilename, modelName):
+    # Load the age classification pipeline
+    # The code below should be placed in the main part of the program
+    age_classifier = pipeline(""image-classification"", 
+                              model=modelName)
+    
+    image_name = imgFilename
+    image_name = Image.open(image_name).convert(""RGB"")
+    
+    # Classify age
+    age_predictions = age_classifier(image_name)
+
+    return age_predictions
+
+
+def output_msg(age_predictions):
+    st.write(age_predictions)
+    age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
+    
+    # Display results
+    st.write(""Predicted Age Range:"")
+    st.write(f""Age range: {age_predictions[0]['label']}"")
+    
+    st.write(""Done"")    
+
+
+
+def main():
+    # Streamlit UI
+    st.header(""Title: Age Classification using ViT"")
+    
+
+
+#    age_predictions = ageClassifier(""middleagedMan.jpg"",
+#                                   ""prithivMLmods/Age-Classification-SigLIP2"")
+    age_predictions = ageClassifier(""middleagedMan.jpg"",
+                                   ""akashmaggon/vit-base-age-classification"")
+    output_msg(age_predictions)
+
+  
+
+
+
+# main part
+if __name__ == ""__main__"":
     main()
+
+"
